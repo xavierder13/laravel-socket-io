ab9ef29d73040d63852204c048956180aa86421c
xavierder13 <xavierder26@gmail.com>
2021-04-28 21:25:47 +0800

    install vuetify

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,22 +1,9 @@
 <template>
-  <button @click="getData()">Click</button>
+  <v-app>
+    <!-- Content -->
+    <router-view />
+  </v-app>
 </template>
 
 <script>
-export default {
-  data() {
-    return {};
-  },
-  methods: {
-    getData() {
-      // console.log("clicked!");
-      this.$socket.emit("sendData", "Message Sent!");
-    },
-  },
-  mounted() {
-    this.$options.sockets.sendData = (data) => {
-      console.log(data);
-    };
-  },
-};
 </script>
---
+++ resources/js/router.js
@@ -0,0 +1,56 @@
+import Vue from 'vue';
+import Router from 'vue-router';
+import Home from './views/Home.vue';
+import Login from './auth/Login.vue';
+import PageNotFound from './404/PageNotFound.vue';
+import Unauthorize from './401/Unauthorize.vue';
+
+Vue.use(Router);
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: Home,
+    children: [
+      {
+        path: '/unauthorize',
+        name: 'unauthorize',
+        component: Unauthorize,
+      }
+    ],
+    beforeEnter(to, from, next) {
+
+      if (localStorage.getItem('access_token')) {
+        next();
+      }
+      else {
+        next('/login');
+      }
+    }
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: Login,
+    beforeEnter(to, from, next) {
+      
+      if (localStorage.getItem('access_token')) {
+        next('/');
+      }
+      else {
+        next();
+      }
+    }
+  },
+  {
+    path: '*',
+    component: PageNotFound,
+  },
+];
+
+const router = new Router({
+  routes: routes
+});
+
+export default router;
